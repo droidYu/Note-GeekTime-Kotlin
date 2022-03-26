120ecfe69dcd4ebb5c3b184134b0751131c31082
droidYu <1370576461@qq.com>
2022-03-26 14:57:25 +0800

    TextProcessorV1

---
+++ Code/TextProcessor/src/main/kotlin/TextProcessorV1.kt
@@ -0,0 +1,41 @@
+fun main() {
+    val text="Kotlin is my favorite language. I love Kotlin!"
+    val processorV1=TextProcessorV1()
+    println( processorV1.processText(text))
+}
+
+class TextProcessorV1 {
+    fun processText(text: String): List<WordFreq> {
+        val cleaned = clend(text)
+        val words = cleaned.split(' ')
+        val map = getWordCount(words)
+        return sortByFrequency(map)
+    }
+
+    private fun sortByFrequency(map: Map<String, Int>): List<WordFreq> {
+        val list= mutableListOf<WordFreq>()
+        for (entry in map) {
+            if (entry.key=="")
+                continue
+            list.add(WordFreq(entry.key,entry.value))
+        }
+        list.sortByDescending {
+            it.frequency
+        }
+        return list
+    }
+
+    private fun getWordCount(words: List<String>): Map<String, Int> {
+        val map= hashMapOf<String,Int>()
+        for (word in words) {
+            val count=map.getOrDefault(word,0)
+            map[word]=count+1
+        }
+        return map
+    }
+
+    private fun clend(text: String): String {
+        return text.replace("[^A-Za-z]".toRegex()," ").trim()
+    }
+}
+data class WordFreq(val word: String, val frequency: Int)
